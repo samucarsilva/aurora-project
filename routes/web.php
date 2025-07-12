88b7f4b5f8f0f838e821a49eb379c061ae5f2f81
samucarsilva <samucarsilva007@gmail.com>
2025-07-11 21:18:33 -0300

    Feat: Adicionar Rotas Para as Views

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/about', function () {
+    return view('about', ['page' => 'about']);
+})->name('about');
+
+
+Route::get('/courses', function () {
+    return view('courses', ['page' => 'courses']);
+})->name('courses');
+
+
 Route::get('/', function () {
     return view('home', ['page' => 'home']);
 })->name('home');
 
+
+Route::get('/login', function () {
+    return view('login', ['page' => 'login']);
+})->name('login');
+
+
+Route::get('/register', function () {
+    return view('register', ['page' => 'register']);
+})->name('register');
