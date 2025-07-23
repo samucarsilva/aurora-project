83a69eb75f0595f804ddb203e6d8f1fa16c8b27d
samucarsilva <samucarsilva007@gmail.com>
2025-07-22 23:50:13 -0300

    Feat: Adicionar Feedback de Erro no Formulário de Cadastro
    Este commit aprimora a experiência do usuário na tela de registro, fornecendo um feedback visual e funcional mais claro:
    
    — Foram adicionadas mensagens de erro para cada campo de input do formulário
    — Foram adicionadas classes CSS para indicar visualmente campos com erro

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,7 +5,7 @@
 
         <div class="form-container">
 
-            <form class="form-card" action="" method="POST">
+            <form class="form-card" action="{{ route('register.store') }}" method="POST">
                 @csrf
 
 
@@ -17,20 +17,50 @@
                 <div class="form-main">
 
                     <div class="form-group">
-                        <input class="form-input" type="text" name="name" id="name" autocomplete="off" placeholder=" ">
-                        <label class="form-label" for="name"> Name </label>
+
+                        <input class="form-input @error('name') border-danger @enderror" type="text" name="name" id="name" value="{{ old('name') }}" autocomplete="off" placeholder=" ">
+                        <label class="form-label @error('name') text-danger @enderror" for="name"> Name </label>
+
+                        @error('name')
+
+                            <div class="d-block invalid-feedback">
+                                 <p class="font-size-1 text-danger"> {{ $message }} </p>
+                            </div>
+
+                        @enderror
+
                     </div>
 
 
                     <div class="form-group">
-                        <input class="form-input" type="email" name="email" id="email" autocomplete="off" placeholder=" ">
-                        <label class="form-label" for="email"> E-Mail </label>
+
+                        <input class="form-input @error('email') border-danger @enderror" type="email" name="email" id="email" value="{{ old('email') }}" autocomplete="off" placeholder=" ">
+                        <label class="form-label @error('email') text-danger @enderror" for="email"> E-Mail </label>
+
+                        @error('email')
+
+                            <div class="d-block invalid-feedback">
+                                 <p class="font-size-1 text-danger"> {{ $message }} </p>
+                            </div>
+
+                        @enderror
+
                     </div>
 
 
                     <div class="form-group">
-                        <input class="form-input" type="password" name="password" id="password" autocomplete="off" placeholder=" ">
-                        <label class="form-label" for="password"> Password </label>
+
+                        <input class="form-input @error('password') border-danger @enderror" type="password" name="password" id="password" placeholder=" ">
+                        <label class="form-label @error('password') text-danger @enderror" for="password"> Password </label>
+
+                        @error('password')
+
+                            <div class="d-block invalid-feedback">
+                                 <p class="font-size-1 text-danger"> {{ $message }} </p>
+                            </div>
+
+                        @enderror
+
                     </div>
 
 
@@ -38,10 +68,18 @@
 
                         <div class="form-checkbox">
 
-                            <input class="form-input checkbox" type="checkbox" name="terms" id="terms">
-                            <label for="terms"> Accept <a class="font-semibold" href="#"> Terms of Use </a> </label>
+                            <input class="form-input checkbox @error('terms') danger-outlined @enderror" type="checkbox" name="terms" id="terms">
+                            <label for="terms checkbox @error('terms') text-danger @enderror"> Accept <a class="font-semibold" href="#"> Terms of Use </a> </label>
 
                         </div>
+
+                            @error('terms')
+
+                                <div class="d-block invalid-feedback">
+                                    <p class="font-size-1 text-danger"> {{ $message }} </p>
+                                </div>
+
+                            @enderror
 
                     </div>
 
