111e6628f10ce2a96d78c47d43c3b067d1684c94
samucarsilva <samucarsilva007@gmail.com>
2025-08-14 22:56:20 -0300

    Feat: Implementar Feedback de Erro no Formulário de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,15 +20,33 @@
             <div class="form-main gap-5">
 
                 <div class="form-group">
-                    <input class="form-input" type="email" name="email" id="email" value="{{ old('email') }}" autocomplete="email" placeholder=" ">
-                    <label class="form-label" for="email"> E-Mail </label>
+
+                    <input class="form-input @error('email') border-danger text-danger @enderror" type="email" name="email" id="email" value="{{ old('email') }}" autocomplete="email" placeholder=" ">
+                    <label class="form-label @error('email') text-danger @enderror" for="email"> E-Mail </label>
+
+                    @error('email')
+
+                        <div class="d-block invalid-feedback">
+                            <p class="font-size-1 text-danger"> {{ $message }} </p>
+                        </div>
+
+                    @enderror
+
                 </div>
 
 
                 <div class="form-group">
 
-                    <input class="form-input" type="password" name="password" id="password" autocomplete="current-password" placeholder=" ">
-                    <label class="form-label" for="password"> Password </label>
+                    <input class="form-input @error('password') border-danger text-danger @enderror" type="password" name="password" id="password" autocomplete="current-password" placeholder=" ">
+                    <label class="form-label @error('password') text-danger @enderror" for="password"> Password </label>
+
+                    @error('password')
+
+                        <div class="d-block invalid-feedback">
+                            <p class="font-size-1 text-danger"> {{ $message }} </p>
+                        </div>
+
+                    @enderror
 
                     <div class="display-flex justify-end pt-3">
                         <a class="button-link" href="{{ route('forgot') }}"> Forgot Password </a>
